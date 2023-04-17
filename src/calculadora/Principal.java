fa62a0675cc14aeb9dde46227a496924b218e18a
Antonio Martinez Lopez <nonomartinezlopez2004@gmail.com>
2023-04-17 09:31:00 +0200

    case resto añadido

--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -13,11 +13,12 @@ public class Principal {
 			System.out.println("\n===================");
 			System.out.println("Elija una operacion");
 			System.out.println("===================");
-			System.out.println("(1)SUMAR        (+)");
-			System.out.println("(2)RESTAR       (-)");
-			System.out.println("(3)MULTIPLICAR  (*)");
-			System.out.println("(4)DIVIDIR      (/)");
-			System.out.println("(5)RESTO        (%)");
+			System.out.println("(1) SUMAR       (+)");
+			System.out.println("(2) RESTAR      (-)");
+			System.out.println("(3) MULTIPLICAR (*)");
+			System.out.println("(4) DIVIDIR     (/)");
+			System.out.println("(5) RESTO       (%)");
+			System.out.println("(0) SALIR");
 			System.out.println("===================");
 			menu = Leer.datoInt();
 			
@@ -53,6 +54,14 @@ public class Principal {
 				num2=Leer.datoDouble();
 				resul=num1/num2;
 				System.out.printf("El resultado de la división es %.2f", resul);
+				break;
+			case 5:
+				System.out.println("Diga el primer número");
+				num1=Leer.datoDouble();
+				System.out.println("Diga el segundo número");
+				num2=Leer.datoDouble();
+				resul=num1%num2;
+				System.out.printf("El resto de la división es %.2f", resul);
 				break;
 			}
 		}while(menu!=0);
