41a37cc8b52113f0a31ecad17ccff3b244e159e9
Antonio Martinez Lopez <nonomartinezlopez2004@gmail.com>
2023-04-17 09:28:25 +0200

    case dividir añadido

--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -13,11 +13,11 @@ public class Principal {
 			System.out.println("\n===================");
 			System.out.println("Elija una operacion");
 			System.out.println("===================");
-			System.out.println(" SUMAR         (+)");
-			System.out.println(" RESTAR        (-)");
-			System.out.println(" MULTIPLICAR   (*)");
-			System.out.println(" DIVIDIR       (/)");
-			System.out.println(" RESTO         (%)");
+			System.out.println("(1)SUMAR        (+)");
+			System.out.println("(2)RESTAR       (-)");
+			System.out.println("(3)MULTIPLICAR  (*)");
+			System.out.println("(4)DIVIDIR      (/)");
+			System.out.println("(5)RESTO        (%)");
 			System.out.println("===================");
 			menu = Leer.datoInt();
 			
@@ -45,6 +45,14 @@ public class Principal {
 				num2=Leer.datoDouble();
 				resul=num1*num2;
 				System.out.printf("El resultado de la multiplicación es %.2f", resul);
+				break;
+			case 4:
+				System.out.println("Diga el primer número");
+				num1=Leer.datoDouble();
+				System.out.println("Diga el segundo número");
+				num2=Leer.datoDouble();
+				resul=num1/num2;
+				System.out.printf("El resultado de la división es %.2f", resul);
 				break;
 			}
 		}while(menu!=0);
